44d6c76a5cc92f5be7b91f9fc00c6ccea83a9f04
root <root@rov-bottom.(none)>
2015-11-12 19:08:22 +0000

    fixed some strange bugs for 6-engine ROV

--- src/bottom/remote_init.sh
+++ src/bottom/remote_init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export ROS_MASTER_URI=http://rov-surface:11311
-source "/home/ubuntu/rov/devel/setup.bash"
+source "/root/rov/devel/setup.bash"
 
 exec "$@"
--- src/bottom/scripts/bottom.py
+++ src/bottom/scripts/bottom.py
@@ -14,63 +14,48 @@ class MotorController():
     self.stop_time  = 0.0015
     self.time_delta = 0.0004
     self.port = port
-    self.frequency = 100
-    self.period = 1.0/self.frequency
-    PWM.stop(port)
-
-  def start(self):
-    PWM.start(self.port, 100*self.stop_time/self.period, self.frequency)
+    self.frequency = 100.0
+    PWM.start(self.port, 0, self.frequency)
 
   def set_power(self, power):
     if abs(power) > self.MAX:
       power = math.copysign(self.MAX, power)
     time = self.stop_time + power*self.time_delta
-    percent = 100*time/self.period
-    print self.port, percent
+    percent = 100.0*time*self.frequency
     PWM.set_duty_cycle(self.port, percent)
-
-  def cleanup(self):
-    PWM.stop(self.port)
 
 class RovController:
   def __init__(self):
     self.motors = {}
-    self.motors["FL"] = MotorController("P8_13") #P2B
+    self.motors["FL"] = MotorController("P9_14")
     self.motors["FR"] = MotorController("P9_16")
-    """
     self.motors["TF"] = MotorController("P9_21")
     self.motors["TB"] = MotorController("P9_22")
-    self.motors["LF"] = MotorController("P9_42") 
-    self.motors["LB"] = MotorController("P9_14")
-    """
-    PWM.cleanup()
-    for motor in self.motors.values():
-      motor.start()
+    self.motors["LF"] = MotorController("P8_13") 
+    self.motors["LB"] = MotorController("P8_19")
+    time.sleep(1)
 
-    rospy.init_node('bottom_side', anonymous=True)
+    for k in self.motors.keys():
+        self.motors[k].set_power(0)
     rospy.Subscriber("rov_control", ControlData, self.callback)
 
-  def __exit__(self, exc_type, exc_value, traceback):
-    for motor in self.motors.values():
-      motor.cleanup()
-    PWM.cleanup()
-
   def callback(self, data):
-    print data 
     if data.stop:
       for motor in self.motors.values():
         motor.set_power(0)
     else:
       self.motors["FL"].set_power(data.forward_left) 
       self.motors["FR"].set_power(data.forward_right) 
-"""
       self.motors["TF"].set_power(data.top_front) 
       self.motors["TB"].set_power(data.top_back) 
       self.motors["LF"].set_power(data.lag_front) 
       self.motors["LB"].set_power(data.lag_back) 
-"""
+
 if __name__ == '__main__':
+    rospy.init_node('bottom_side', anonymous=True)
     core = RovController()
 
-    while True:
+    while not rospy.is_shutdown():
         pass
+
+    PWM.cleanup()
